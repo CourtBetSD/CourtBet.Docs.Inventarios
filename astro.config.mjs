61482c11c4696c868f648896c42d0cfd70f4dcab
David Mendez Guardado <demg@outlook.com>
2024-09-26 11:12:31 -0600

    fix: Updated
    Refs: @DEMG-DEV

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://courtbetsd.github.io/CourtBet.Docs.Inventarios/',
 	integrations: [
 		starlight({
 			title: 'Sistema de Inventarios',
